997488cd2a2dd45bda1f1246d0ca7c8a268657fb
Mark Nielsen <marknielsen@focus54.com>
2014-05-25 21:16:11 +0000

    dynamic home page - rails mvc

---
+++ app/models/owner.rb
@@ -0,0 +1,29 @@
+class Owner
+
+  def name
+    name = 'Foobar Kadigan'
+  end
+
+  def birthdate
+    birthdate = Date.new(1964, 05, 28)
+  end
+
+  def countdown
+    today = Date.today
+    birthday = Date.new(today.year, birthdate.month, birthdate.day)
+    if birthday > today
+      countdown = (birthday - today).to_i
+    else
+      countdown = (birthday.next_year - today).to_i
+    end
+  end
+  
+  def favoriteFood
+    favoriteFood = "Bananas"
+  end
+  
+  def whatsup
+    whatsup = "Well, my name is " + name + " and my fav food is " + favoriteFood
+  end
+
+end
